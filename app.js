dbbeeadde24a7648590866f6783d5e64af399400
sneha singh <81037395+i-am-a-codegirl@users.noreply.github.com>
2021-08-27 21:23:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTransalationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+console.log("error occured", error)
+alert("something is wrong with server, try after sometime");
+}
+
+function clickHandler() {
+    var inputText = txtInput.value;
+
+    fetch(getTransalationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+
+            
+        })
+
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
